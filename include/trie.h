5cfd6f39c05777ae47da9495aadbaeab8eb10764
GeorgiiGlazkov <glazkovgeorgii@gmail.com>
2025-12-10 12:09:20 +0100

    Implement getChildIndexFromLetter()

--- include/trie.h
+++ include/trie.h
@@ -1,4 +1,8 @@
+#ifndef TRIE_H
+#define TRIE_H
+
 #include <stdbool.h>
+#include <stdio.h>
 
 #define MAX_CHILDREN 27
 
@@ -12,3 +16,6 @@ void addWord(TrieNode* root, char* word);
 
 bool findWord(TrieNode* root, char* word);
 
+void deleteWord(TrieNode* root, char* word);
+
+#endif // TRIE_H
---
+++ include/utility.h
@@ -0,0 +1,8 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <stddef.h>
+
+size_t getChildIndexFromLetter(char letter);
+
+#endif // UTILITY_H
