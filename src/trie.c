dd40c4a0430548d419460ae65ff55ee81b0741dd
GeorgiiGlazkov <glazkovgeorgii@gmail.com>
2025-12-16 20:23:33 +0100

    Implement deleteWord()

--- src/trie.c
+++ src/trie.c
@@ -44,7 +44,7 @@ bool findWord(TrieNode* root, char* word) {
     size_t len = strlen(word);
     TrieNode* currTrieNode = root;
 
-    for (size_t i = 0; i < len; i++) {
+    for (size_t i = 0; i < len - 1; i++) {
         if (currTrieNode == NULL) return false;
 
         currTrieNode = currTrieNode->children[getChildIndexFromLetter(word[i]);
@@ -52,17 +52,48 @@ bool findWord(TrieNode* root, char* word) {
 
     return currTrieNode->isTerminal;
 }
-/*
+
 void deleteWord(TrieNode* root, char* word) {
     size_t len = strlen(word);
     TrieNode* currTrieNode = root;
     TrieNode* lastUnsafeToDeleteNode = NULL;
+	size_t lastUnsafeLetterIndex = 0;
     for (int i = 0; i < len; i++)
     {
-        if (currTrieNode->children[getChildIndexFromLetter(word[i])] != NULL) {
+        if (currTrieNode->children[getChildIndexFromLetter(word[i])] == NULL) {
             printf("Deletion cancelled: no such word (%s)\n", word);
             return;
         }
+
+		size_t children = 0;
+
+		for (int i = 0; i < MAX_CHILDREN; i++) {
+			if (currTrieNode->children[i] != NULL) children++;
+		}
+
+		if (children > 1 || (currTrieNode->isTerminal == 1 && i < len - 1)) {
+			lastUnsafeToDeleteNode = currTrieNode;
+			lastUnsafeLetterIndex = i;
+		}
+
+		currTrieNode = currTrieNode->children[getChildIndexFromLetter(word[i])];
     }
+
+	size_t children = 0;
+
+	for (int i = 0; i < MAX_CHILDREN; i++) {
+		if (currTrieNode->children[i] != NULL) children++;
+	}
+
+	if (children > 0) {
+	currTrieNode->isTerminal = false;
+	}else {
+	freeTrie(lastUnsafeToDeleteNode->children[getChildIndexFromLetter(word[i])];
+	}
+
+
+
+
+
 }
-*/
+
